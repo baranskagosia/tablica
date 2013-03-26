275d6ec11d6fb4a2fb8ed33fe9df2ca99e99e260
Marcin Adamczyk <adamczyk.marcin@outlook.com>
2013-03-26 20:44:13 +0100

    Base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tablica::Application.config.secret_token = '07c30433bf38d1c46f13bf245906627eb56e4f7ecd98465ae58ca90f1621e11b2a6947e6f7e6a51152542e814ad3ced0f7e52f97a73585c21834ffed5c0993f6'
